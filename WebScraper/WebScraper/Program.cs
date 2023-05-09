c576c5831ff483ae547442e870bd5da50476ac38
Katakuna2011 <125313263+Katakuna2011@users.noreply.github.com>
2023-05-09 17:43:51 -0300

    Nova Atualização x86
    Agora vai!!

--- WebScraper/WebScraper/Program.cs
+++ WebScraper/WebScraper/Program.cs
@@ -3,14 +3,27 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.Write("Digite a porcentagem alvo: ");
-        int alvo = int.Parse(Console.ReadLine());
+        try
+        {
+            Console.Write("Digite a porcentagem alvo: ");
+            int alvo = int.Parse(Console.ReadLine());
 
-        Console.Write("Digite o destino da mensagem (conforme salvo no WhatsApp): ");
-        string destino = Console.ReadLine();
-        var web = new WebScraperBlaze();
-        web.GetData("https://blaze.com/pt/games/crash", alvo, destino);
-        
+            Console.Write("Digite o destino da mensagem (conforme salvo no WhatsApp): ");
+            string destino = Console.ReadLine();
+            var web = new WebScraperBlaze();
+            web.GetData("https://blaze.com/pt/games/crash", alvo, destino);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Houve um erro...");
+            throw;
+        }
+        finally
+        {
+            Console.ReadKey();
+        }
+
+
     }
 }
 
