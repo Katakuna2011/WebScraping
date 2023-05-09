e1d8cc41e1f072272325ded7eae8db8c08a8e941
Katakuna2011 <125313263+Katakuna2011@users.noreply.github.com>
2023-05-09 14:34:53 -0300

    program.cs update

--- WebScraper/WebScraper/Program.cs
+++ WebScraper/WebScraper/Program.cs
@@ -7,7 +7,7 @@ internal class Program
         int alvo = int.Parse(Console.ReadLine());
 
         Console.Write("Digite o destino da mensagem (conforme salvo no WhatsApp): ");
-        string destino = Console.ReadLine();D
+        string destino = Console.ReadLine();
         var web = new WebScraperBlaze();
         web.GetData("https://blaze.com/pt/games/crash", alvo, destino);
         
